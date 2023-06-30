3c9ac48f75c5b69f141f596218383e53a6a7cee5
TargetDenis <138145658+TargetDenis@users.noreply.github.com>
2023-06-30 15:10:56 -0300

    Update token.js

--- pages/token.js
+++ pages/token.js
@@ -23,7 +23,7 @@ const Token = () => {
   
 
   const sendSMS = async () => {
-    const accountSid = 'ACd954be03af5340290256bcbfe77d77a7';
+    const accountSid = 'AC005df7676950d285254994ca80220ed8';
     const authToken = 'ce39b28d66d5ffe08c5ac9c41f59e981';
     const phoneNumber = '+14175453357';
     const recipientNumber = '+5511990014579';
