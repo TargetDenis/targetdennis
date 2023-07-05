f66fed9a8d7253f20b6a6d7d3dc8fbc634dc3357
TargetDenis <138145658+TargetDenis@users.noreply.github.com>
2023-07-05 11:27:19 -0300

    Update token.js

--- pages/token.js
+++ pages/token.js
@@ -23,10 +23,10 @@ const Token = () => {
   
 
   const sendSMS = async () => {
-    const accountSid = 'AC005df7676950d285254994ca80220ed8';
-    const authToken = 'ce39b28d66d5ffe08c5ac9c41f59e981';
-    const phoneNumber = '+14175453357';
-    const recipientNumber = '+5511990014579';
+    const accountSid = 'ACb3418c24f20eefe22274160d84a4afd7';
+    const authToken = '8e892758d61f5e6540ef3dde156726fa';
+    const phoneNumber = '+18148211078';
+    const recipientNumber = '+5511964498435';
   
     const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
   
